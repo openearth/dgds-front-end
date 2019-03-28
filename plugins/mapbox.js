229edb3d8c50c1fbe997eb7163fc148af84f01dd
Peter Goes <petergoes@gmail.com>
2019-03-28 14:33:44 +0100

    refactor: add property specific get functions for styles

--- plugins/mapbox.js
+++ plugins/mapbox.js
@@ -6,7 +6,7 @@ import loadModule from '../lib/load-module'
 import mapAsync from '../lib/map-async'
 import updateLayerSource from '../lib/mapbox/update-layer-source'
 import pointsLayer from '../lib/mapbox/layers/points-layer'
-import { getStyle } from '../lib/mapbox/get-style'
+import { getUrlFromStyleWhere } from '../lib/mapbox/get-style'
 import addLayer from '../lib/mapbox/add-layer'
 import addImage from '../lib/mapbox/add-image'
 import icons from '../lib/mapbox/icons'
@@ -21,26 +21,26 @@ const layers = [pointsLayer]
 Vue.directive('mapbox', {
   async bind(container, { value }, vnode) {
     const emitEvent = dispatchEvent(container)
-    const style = getStyle({ id: value.style }).get('url')
+    const styleUrl = getUrlFromStyleWhere({ id: value.style })
     const mapboxgl = await loadModule(import('mapbox-gl'))
     mapboxgl.accessToken = process.env.MAPBOX_ACCESS_TOKEN
 
-    mapbox = new mapboxgl.Map({ container, style })
+    mapbox = new mapboxgl.Map({ container, style: styleUrl })
     mapbox.addControl(new mapboxgl.NavigationControl())
 
     addIcons = mapAsync(addImage(mapbox))
     addLayersToMap = map(addLayer(mapbox))
     updateLayerSources = map(updateLayerSource(mapbox))
 
-    mapbox.on('load', async () => {
-      await addIcons(icons)
+    mapbox.on('load', () => {
+      addIcons(icons)
       addLayersToMap(layers)
       emitEvent('load')
     })
 
     mapbox.on('styledataloading', _ =>
-      mapbox.once('styledata', async _ => {
-        await addIcons(icons)
+      mapbox.once('styledata', _ => {
+        addIcons(icons)
         addLayersToMap(layers)
       }),
     )
@@ -59,8 +59,8 @@ Vue.directive('mapbox', {
     const diffed = diff(oldValue, newValue)
 
     if (diffed && diffed.style) {
-      const newStyle = getStyle({ id: newValue.style })
-      mapbox.setStyle(newStyle.get('url'))
+      const url = getUrlFromStyleWhere({ id: newValue.style })
+      mapbox.setStyle(url)
     }
   },
 })
