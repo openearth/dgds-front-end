ecdc32054a13300fdb9ac756e7dfdd6580a4fc0b
Peter Goes <petergoes@gmail.com>
2019-04-03 14:08:39 +0200

    refactor(echarts): keep track of resize handlers and remove on destroy

--- plugins/echarts.js
+++ plugins/echarts.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import echarts from 'echarts'
 import throttle from 'lodash/throttle'
 
+const resizeHandlers = {}
+
 Vue.directive('echarts', {
   inserted(el, { value }) {
     // on bind initiate echart and set the standard tools for the echart
@@ -9,18 +11,17 @@ Vue.directive('echarts', {
     const chart = echarts.init(el, null, { renderer })
     chart.setOption(data)
 
-    window.addEventListener(
-      'resize',
-      throttle(() => {
-        chart.resize()
-      }, 100),
-    )
+    const resizeHandler = throttle(chart.resize, 100)
+    window.addEventListener('resize', resizeHandler)
+    resizeHandlers[chart.id] = resizeHandler
   },
   update(el, { value: newValue, oldValue }) {
     const chart = echarts.getInstanceByDom(el)
     chart.setOption(newValue.data)
   },
   unbind(el) {
+    const chart = echarts.getInstanceByDom(el)
+    window.removeEventListener('resize', resizeHandlers[chart.id])
     echarts.dispose(el)
   },
 })
