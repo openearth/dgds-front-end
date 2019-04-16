a7b691b6f7715eb0ad4718c947dba1b7304c2cf9
Peter Goes <petergoes@gmail.com>
2019-04-16 13:43:08 +0200

    feat(panel): add panel component

---
+++ components/panel.vue
@@ -0,0 +1,35 @@
+<template>
+  <section class="panel">
+    <header class="panel__header">
+      <slot name="header" />
+    </header>
+
+    <section class="panel__body">
+      <slot />
+    </section>
+
+    <footer class="panel__footer">
+      <slot name="footer" />
+    </footer>
+  </section>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      default: '',
+    },
+  },
+}
+</script>
+
+<style>
+.panel {
+  background-color: var(--color-background);
+  padding: var(--spacing-default);
+  box-shadow: var(--shadow);
+  border-radius: 4px;
+}
+</style>
