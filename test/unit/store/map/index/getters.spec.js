8d7ab79eaa3d3acf38bc11946eebe3dbb28d5419
Peter Goes <petergoes@gmail.com>
2019-04-23 11:08:14 +0200

    test(store/map/index): clean up tests

--- test/unit/store/map/index/getters.spec.js
+++ test/unit/store/map/index/getters.spec.js
@@ -1,20 +1,21 @@
+import update from 'lodash/fp/update'
 import { getters } from '../../../../../store/map/index.js'
 
-test('knownDatasetIds returns object of known ids of datasets', () => {
-  const state = {
-    datasets: {
-      wl: {},
-      wd: {},
-    },
-  }
-  const ids = ['wl', 'wd']
-  const result = getters.knownDatasetIds(state)
-  expect(result).toHaveLength(2)
-  expect(typeof result).toBe('object')
-  expect(result).toEqual(ids)
+describe('knownDatasetIds', () => {
+  test('returns array of known dataset ids', () => {
+    const state = {
+      datasets: {
+        wl: {},
+        wd: {},
+      },
+    }
+    const ids = ['wl', 'wd']
+    const result = getters.knownDatasetIds(state)
+    expect(result).toEqual(ids)
+  })
 })
 
-test('knownLocationIds returns object of known location ids', () => {
+describe('knownLocationIds', () => {
   const state = {
     datasets: {
       wl: {
@@ -29,144 +30,160 @@ test('knownLocationIds returns object of known location ids', () => {
       },
     },
   }
-  let result = getters.knownLocationIds(state)
-  expect(result).toHaveLength(0)
-  expect(typeof result).toBe('object')
-  state.datasets.wl.locations.features.push({
-    properties: {
-      locationId: 'WL-Loc1',
-    },
-  })
-  result = getters.knownLocationIds(state)
-  expect(result).toHaveLength(1)
-  expect(result).toEqual(['WL-Loc1'])
 
-  state.datasets.wd.locations.features.push({
-    properties: {
-      locationId: 'WD-Loc2',
-    },
+  test('returns an empty array when no locations are present', () => {
+    const result = getters.knownLocationIds(state)
+    expect(result).toHaveLength(0)
   })
-  result = getters.knownLocationIds(state)
-  expect(result).toHaveLength(2)
-  expect(result).toEqual(['WL-Loc1', 'WD-Loc2'])
+
+  test('returns array with one location if only one id is present', () => {
+    const newState = update(
+      'datasets.wl.locations.features',
+      _ => [{ properties: { locationId: 'WL-Loc1' } }],
+      state,
+    )
+    const result = getters.knownLocationIds(newState)
+    expect(result).toEqual(['WL-Loc1'])
+  })
+
+  test('returns array with multiple locations if multiple ids are present', () => {
+    let newState = update(
+      'datasets.wl.locations.features',
+      _ => [{ properties: { locationId: 'WL-Loc1' } }],
+      state,
+    )
+    newState = update(
+      'datasets.wd.locations.features',
+      _ => [{ properties: { locationId: 'WD-Loc2' } }],
+      newState,
+    )
+    const result = getters.knownLocationIds(newState)
+    expect(result).toEqual(['WL-Loc1', 'WD-Loc2'])
+  })
 })
 
-test('activeDatasets returns object with the active dataset', () => {
+describe('activeDatasets', () => {
   const state = {
     activeDatasetIds: ['wl'],
     datasets: {
       wl: {
         data: 'test-wl',
       },
-      wd: {},
-    },
-  }
-  let result = getters.activeDatasets(state)
-  expect(typeof result).toBe('object')
-  expect(result).toEqual([state.datasets.wl])
-
-  state.activeDatasetIds.push('wd')
-  result = getters.activeDatasets(state)
-  expect(result).toEqual([state.datasets.wl, state.datasets.wd])
-
-  // state.activeDatasetIds.push('test')
-  // result = getters.activeDatasets(state)
-  // expect(result).toEqual([state.datasets.wl, state.datasets.wd])
-})
-
-test('activeDatasetsLocations returns object of datasets for the active locations', () => {
-  const state = {
-    activeLocationIds: ['WL-Loc1'],
-    activeDatasets: [
-      {
-        locations: {
-          features: [
-            {
-              properties: {
-                locationId: 'WL-Loc1',
-              },
-            },
-          ],
-        },
-        pointData: {
-          'WL-Loc1': {
-            data: 'test-wl-loc1',
-          },
-          'WL-Loc2': {
-            data: 'test-wl-loc2',
-          },
-        },
-      },
-    ],
-  }
-  const result = getters.activeDatasetsLocations(state, state)
-  expect(typeof result).toBe('object')
-  expect(result).toEqual([
-    { features: [{ properties: { active: true, locationId: 'WL-Loc1' } }] },
-  ])
-})
-
-test('activeDatasetsLocations ignores unknown datasets ids', () => {
-  const state = {
-    activeLocationIds: ['WL-Loc1'],
-    activeDatasetIds: ['wl'],
-    datasets: { wl: { pointData: { foo: 'bar' } } },
-  }
-  const result = getters.activePointDataPerDataset(state, state)
-  expect(result).toEqual({ 'WL-Loc1': [{}] })
-})
-
-test('activePointDataPerDataset returns object of pointdata for the active locations', () => {
-  const state = {
-    activeLocationIds: ['WL-Loc1'],
-    activeDatasetIds: ['wl'],
-    datasets: {
-      wl: {
-        locations: {
-          features: [
-            {
-              properties: {
-                locationId: 'WL-Loc1',
-              },
-            },
-          ],
-        },
-        pointData: {
-          'WL-Loc1': {
-            data: 'test-wl-loc1',
-          },
-          'WL-Loc2': {
-            data: 'test-wl-loc2',
-          },
-        },
-      },
       wd: {
-        locations: {
-          features: [],
-        },
+        data: 'test-wd',
       },
     },
   }
-  const result = getters.activePointDataPerDataset(state)
-  expect(typeof result).toBe('object')
-  expect(result['WL-Loc1']).toEqual([
-    {
-      data: 'test-wl-loc1',
-    },
-  ])
+  test('returns array with one item when one active dataset is present', () => {
+    const result = getters.activeDatasets(state)
+    expect(result).toEqual([state.datasets.wl])
+  })
+  test('returns array with multiple items when multiple active datasets are present', () => {
+    const newState = update('activeDatasetIds', _ => ['wl', 'wd'], state)
+    const result = getters.activeDatasets(newState)
+    expect(result).toEqual([state.datasets.wl, state.datasets.wd])
+  })
 })
 
-test('datasetsInActiveTheme returns array of datasets for the current theme', () => {
-  const state = {
-    activeDatasetIds: ['bar'],
-    datasets: {
-      foo: { metadata: { id: 'foo' } },
-      bar: { metadata: { id: 'bar' } },
-    },
-  }
-  const result = getters.datasetsInActiveTheme(state)
-  expect(result).toEqual([
-    { id: 'foo', visible: false },
-    { id: 'bar', visible: true },
-  ])
+describe('activeDatasetsLocations', () => {
+  test('returns array of datasets for the active locations', () => {
+    const state = {
+      activeLocationIds: ['WL-Loc1'],
+      activeDatasets: [
+        {
+          locations: {
+            features: [
+              {
+                properties: {
+                  locationId: 'WL-Loc1',
+                },
+              },
+            ],
+          },
+          pointData: {
+            'WL-Loc1': {
+              data: 'test-wl-loc1',
+            },
+            'WL-Loc2': {
+              data: 'test-wl-loc2',
+            },
+          },
+        },
+      ],
+    }
+    const result = getters.activeDatasetsLocations(state, state)
+    expect(result).toEqual([
+      { features: [{ properties: { active: true, locationId: 'WL-Loc1' } }] },
+    ])
+  })
+})
+
+describe('activeDatasetsLocations', () => {
+  test('ignores unknown datasets ids', () => {
+    const state = {
+      activeLocationIds: ['WL-Loc1'],
+      activeDatasetIds: ['wl'],
+      datasets: { wl: { pointData: { foo: 'bar' } } },
+    }
+    const result = getters.activePointDataPerDataset(state, state)
+    expect(result).toEqual({ 'WL-Loc1': [{}] })
+  })
+})
+
+describe('activePointDataPerDataset', () => {
+  test('returns array of pointdata for the active locations', () => {
+    const state = {
+      activeLocationIds: ['WL-Loc1'],
+      activeDatasetIds: ['wl'],
+      datasets: {
+        wl: {
+          locations: {
+            features: [
+              {
+                properties: {
+                  locationId: 'WL-Loc1',
+                },
+              },
+            ],
+          },
+          pointData: {
+            'WL-Loc1': {
+              data: 'test-wl-loc1',
+            },
+            'WL-Loc2': {
+              data: 'test-wl-loc2',
+            },
+          },
+        },
+        wd: {
+          locations: {
+            features: [],
+          },
+        },
+      },
+    }
+    const result = getters.activePointDataPerDataset(state)
+    expect(result['WL-Loc1']).toEqual([
+      {
+        data: 'test-wl-loc1',
+      },
+    ])
+  })
+})
+
+describe('datasetsInActiveTheme', () => {
+  test('returns array of datasets for the current theme', () => {
+    const state = {
+      activeDatasetIds: ['bar'],
+      datasets: {
+        foo: { metadata: { id: 'foo' } },
+        bar: { metadata: { id: 'bar' } },
+      },
+    }
+    const result = getters.datasetsInActiveTheme(state)
+    expect(result).toEqual([
+      { id: 'foo', visible: false },
+      { id: 'bar', visible: true },
+    ])
+  })
 })
--- test/unit/store/map/index/mutations.spec.js
+++ test/unit/store/map/index/mutations.spec.js
@@ -1,37 +1,45 @@
 import { mutations } from '../../../../../store/map/index.js'
 
-test('setActiveDatasetIds updates state.activeDatasetIds with ids in payload', () => {
-  const state = {
-    activeDatasetIds: [],
-  }
-  const ids = ['wl', 'wd']
-  mutations.setActiveDatasetIds(state, ids)
-  expect(state.activeDatasetIds).toBe(ids)
+describe('setActiveDatasetIds', () => {
+  test('updates state.activeDatasetIds with ids in payload', () => {
+    const state = {
+      activeDatasetIds: [],
+    }
+    const ids = ['wl', 'wd']
+    mutations.setActiveDatasetIds(state, ids)
+    expect(state.activeDatasetIds).toBe(ids)
+  })
 })
 
-test('clearActiveDatasetIds clears state.activeDatasetIds', () => {
-  const state = {
-    activeDatasetIds: ['wl', 'wd'],
-  }
-  const ids = []
-  mutations.clearActiveDatasetIds(state)
-  expect(state.activeDatasetIds).toEqual(ids)
+describe('clearActiveDatasetIds', () => {
+  test('clears state.activeDatasetIds', () => {
+    const state = {
+      activeDatasetIds: ['wl', 'wd'],
+    }
+    const ids = []
+    mutations.clearActiveDatasetIds(state)
+    expect(state.activeDatasetIds).toEqual(ids)
+  })
 })
 
-test('setActiveLocationIds updates state.setActiveLocationIds with ids in payload', () => {
-  const state = {
-    activeLocationIds: [],
-  }
-  const ids = ['wl', 'wd']
-  mutations.setActiveLocationIds(state, ids)
-  expect(state.activeLocationIds).toBe(ids)
+describe('setActiveLocationIds', () => {
+  test('updates state.setActiveLocationIds with ids in payload', () => {
+    const state = {
+      activeLocationIds: [],
+    }
+    const ids = ['wl', 'wd']
+    mutations.setActiveLocationIds(state, ids)
+    expect(state.activeLocationIds).toBe(ids)
+  })
 })
 
-test('setActiveLocationIds clears state.activeLocationIds', () => {
-  const state = {
-    activeLocationIds: ['wl', 'wd'],
-  }
-  const ids = []
-  mutations.clearActiveLocationIds(state)
-  expect(state.activeLocationIds).toEqual(ids)
+describe('setActiveLocationIds', () => {
+  test('clears state.activeLocationIds', () => {
+    const state = {
+      activeLocationIds: ['wl', 'wd'],
+    }
+    const ids = []
+    mutations.clearActiveLocationIds(state)
+    expect(state.activeLocationIds).toEqual(ids)
+  })
 })
