01e0cba3b23de6563618b8add84e97a7b28f488f
CindyvdVries <cindyvdvries@gmail.com>
2019-04-23 16:32:55 +0200

    adjust mock-api for all params and wms-layer

--- api-mock/redirect-middleware.js
+++ api-mock/redirect-middleware.js
@@ -2,7 +2,9 @@ module.exports = function(req, res, next) {
   if (req.path === '/timeseries') {
     res.redirect(`${req.path}/${req.query.locationCode}/${req.query.datasetId}`)
   } else if (req.path === '/locations') {
-    res.redirect(`${req.path}/${req.query.datasetId}`)
+    // Not all datasetId have  a seperate locations file. Reuse wl file
+    const dId = req.query.datasetId === 'wd' ? 'wd' : 'wl'
+    res.redirect(`${req.path}/${dId}`)
   } else {
     next()
   }
