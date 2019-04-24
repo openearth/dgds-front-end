0bd048ed1e604cfd5088844feeecb6b64a7c97ed
Peter Goes <petergoes@gmail.com>
2019-04-24 10:30:02 +0200

    Update mock so it is paginated

--- api-mock/redirect-middleware.js
+++ api-mock/redirect-middleware.js
@@ -2,7 +2,8 @@ module.exports = function(req, res, next) {
   if (req.path === '/timeseries') {
     res.redirect(`${req.path}/${req.query.locationCode}/${req.query.datasetId}`)
   } else if (req.path === '/locations') {
-    res.redirect(`${req.path}/${req.query.datasetId}`)
+    const paginated = req.query.page ? `-${req.query.page}` : ''
+    res.redirect(`${req.path}/${req.query.datasetId}${paginated}`)
   } else {
     next()
   }
