e9c44ba7753e058d234773940699a1348ebe1304
Peter Goes <petergoes@gmail.com>
2019-04-25 11:51:46 +0200

    Add a timeout on paginated requests
    It makes it easy to test async behaviour to paginated requests

--- api-mock/redirect-middleware.js
+++ api-mock/redirect-middleware.js
@@ -3,7 +3,9 @@ module.exports = function(req, res, next) {
     res.redirect(`${req.path}/${req.query.locationCode}/${req.query.datasetId}`)
   } else if (req.path === '/locations') {
     const paginated = req.query.page ? `-${req.query.page}` : ''
-    res.redirect(`${req.path}/${req.query.datasetId}${paginated}`)
+    setTimeout(() => {
+      res.redirect(`${req.path}/${req.query.datasetId}${paginated}`)
+    }, Number(req.query.page) * 1000)
   } else {
     next()
   }
