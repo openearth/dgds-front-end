5694da2c18f07bec34ba2ec3f1f1689f546e374c
Peter Goes <petergoes@gmail.com>
2019-04-26 15:27:40 +0200

    Add the full dataset wl to the mock

--- api-mock/redirect-middleware.js
+++ api-mock/redirect-middleware.js
@@ -1,11 +1,16 @@
+const path = require('path')
+
 module.exports = function(req, res, next) {
   if (req.path === '/timeseries') {
     res.redirect(`${req.path}/${req.query.locationCode}/${req.query.datasetId}`)
   } else if (req.path === '/locations') {
-    const paginated = req.query.page ? `-${req.query.page}` : ''
-    setTimeout(() => {
-      res.redirect(`${req.path}/${req.query.datasetId}${paginated}`)
-    }, Number(req.query.page) * 1000)
+    const paginated = req.query.page ? `-page=${req.query.page}` : '-page=1'
+    res.sendFile(
+      path.join(
+        __dirname,
+        `${req.path}/${req.query.datasetId}${paginated}.json`,
+      ),
+    )
   } else {
     next()
   }
