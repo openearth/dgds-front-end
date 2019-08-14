6e97f854b3946578017dc54ab6b0b9872e33158e
CindyvdVries <cindyvdvries@gmail.com>
2019-08-14 14:19:48 +0200

    Update site-navigation.vue

--- components/site-navigation.vue
+++ components/site-navigation.vue
@@ -7,10 +7,11 @@
   >
     <ul class="site-navigation__list">
       <li v-for="(theme, key) in getThemes" :key="key">
-        <div class="site-navigation__list-item"
-          >
+        <div class="site-navigation__list-item">
           <UiButtonIcon
-            :class="{ 'site-navigation__list-item--active': checkActive(theme.id) }"
+            :class="{
+              'site-navigation__list-item--active': checkActive(theme.id),
+            }"
             @click="toggleTheme(theme.id)"
           >
             <Icon
@@ -21,7 +22,9 @@
           </UiButtonIcon>
           <span
             class="site-navigation__text h4"
-            :class="{ 'site-navigation__list-item--active': checkActive(theme.id) }"
+            :class="{
+              'site-navigation__list-item--active': checkActive(theme.id),
+            }"
             @click="toggleTheme(theme.id)"
             >{{ theme.name }}</span
           >
@@ -51,11 +54,11 @@ export default {
     ...mapGetters('map/themes', ['getThemes', 'getActiveTheme']),
     timeseriesOpen() {
       const { locationId } = this.$route.params
-      if(locationId) {
+      if (locationId) {
         this.collapse()
       }
       return this.expanded
-    }
+    },
   },
   methods: {
     ...mapMutations('map', ['toggleActiveTheme']),
@@ -162,8 +165,7 @@ export default {
 
 .site-navigation__list-item--active:hover,
 .site-navigation__list-item--active:active,
-.site-navigation__list-item--active:focus,
- {
+.site-navigation__list-item--active:focus {
   color: var(--color-blue-120);
   opacity: 1;
 }
