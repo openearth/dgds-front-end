250b182b83d494f3f7c6e30ec9fe93a11e06f308
Fedor Baart <fedor.baart@deltares.nl>
2019-11-05 22:26:48 +0100

    simplify test'

--- test/unit/lib/mapbox/layers/get-locations-layer.spec.js
+++ test/unit/lib/mapbox/layers/get-locations-layer.spec.js
@@ -1,23 +1,6 @@
 import getVectorLayer from '../../../../../lib/mapbox/layers/get-vector-layer'
-import getColors from '../../../../../lib/styling/colors'
 
-const color = getColors('dark')
-const locationsPaint = {
-  'circle-color': ['case', ['has', 'active'], color.white100, color.pink],
-  'circle-stroke-color': [
-    'case',
-    ['has', 'active'],
-    color.blue60,
-    color.white100,
-  ],
-  'circle-stroke-width': ['case', ['has', 'active'], 4, 1],
-  'circle-pitch-alignment': 'map',
-  'circle-radius': ['interpolate', ['linear'], ['zoom'], 0, 4, 22, 8],
-  'circle-opacity': 0.8,
-  'circle-stroke-opacity': 0.8,
-}
-
-test('returns location layer with default paint', () => {
+test('returns location layer with paint property', () => {
   const locationsLayer = getVectorLayer()
-  expect(locationsLayer).toMatchObject({ paint: locationsPaint })
+  expect(locationsLayer).toHaveProperty('paint')
 })
