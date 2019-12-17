e576b8c136184756a7899b146f3786ed2a0b7cef
Fedor Baart <fedor.baart@deltares.nl>
2019-12-17 17:35:53 +0100

    listen to click events on raster layers

--- components/v-mapbox-components/v-mapbox-raster-layer.vue
+++ components/v-mapbox-components/v-mapbox-raster-layer.vue
@@ -17,8 +17,17 @@ export default {
   data() {
     return {
       map: undefined,
+      // TODO: id or options.id? choose 1
       id: undefined,
     }
+  },
+  computed: {
+    layer() {
+      return this.map.getLayer(this.options.id)
+    },
+    source() {
+      return this.map.getSource(this.options.id)
+    },
   },
   watch: {
     options: {
@@ -35,6 +44,7 @@ export default {
           // If not first remove layer and source before adding new raster layer
           if (source) {
             if (layer) {
+              this.unsubscribe(this.id)
               this.map.removeLayer(this.id)
             }
             this.map.removeSource(this.id)
@@ -44,16 +54,45 @@ export default {
           if (get('source.tiles[0]', newOptions)) {
             this.map.addLayer(newOptions, 'water-border')
           }
+          console.log('subscribing to', newOptions)
+          this.subscribe(newOptions.id)
         }
       },
       deep: true,
     },
+  },
+  mounted() {
+    console.log('mounted', this)
+  },
+  beforeDestroy() {
+    const layer = this.layer
+    if (layer.id && this.map.getLayer(layer.id)) {
+      this.map.off('click', layer.id)
+      this.map.removeLayer(layer.id)
+      this.map.removeSource(layer.id)
+    }
   },
   methods: {
     deferredMountedTo(map) {
       this.map = map
       this.id = this.options.id
       this.map.addLayer(this.options, 'water-border')
+      console.log('subscribing to  click')
+    },
+    subscribe(id) {
+      // Listening on raster layers is not  defined. So we listen to the map and filter events.
+      // https://github.com/mapbox/mapbox-gl-js/issues/1404
+      this.map.on('click', event => {
+        const extendedEvent = {
+          ...event,
+          layer: this.layer,
+          source: this.source,
+        }
+        this.$emit('click', extendedEvent)
+      })
+    },
+    unsubscribe(id) {
+      this.map.off('click', id)
     },
   },
 }
