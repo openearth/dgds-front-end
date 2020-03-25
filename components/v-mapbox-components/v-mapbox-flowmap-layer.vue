1bb5fab96a4d0401401fb0d5f73ec52a1b547040
Fedor Baart <fedor.baart@deltares.nl>
2020-03-25 15:14:58 +0100

    cleanup

--- components/v-mapbox-components/v-mapbox-flowmap-layer.vue
+++ components/v-mapbox-components/v-mapbox-flowmap-layer.vue
@@ -1,14 +1,6 @@
-<template>
-  <div class="offscreen">
-    <!-- <img src="~/assets/glossis-uv.png" class="uv" /> -->
-    <canvas id="uv" width="1024" height="1023" />
-  </div>
-</template>
 <script>
 // https://github.com/mapbox/webgl-wind
 import * as windGl from '@openearth/windgl'
-
-console.log('WindGL', windGl)
 
 export default {
   name: 'VMapboxFlowmapLayer',
@@ -26,24 +18,6 @@ export default {
   mounted () {
   },
   methods: {
-    renderCanvas () {
-      const canvas = this.$el.querySelector('#uv')
-      const ctx = canvas.getContext('2d')
-      ctx.fillStyle = 'rgb(0, 255, 0)'
-      ctx.fillRect(0, 0, 512, 512)
-      ctx.fill()
-      ctx.beginPath()
-      ctx.arc(
-        200, //
-        200,
-        50,
-        0,
-        Math.PI * 2
-      )
-      ctx.fillStyle = 'red'
-      ctx.fill()
-      ctx.closePath()
-    },
     deferredMountedTo (map) {
       const source = windGl.source('glossis/tile.json')
       const layers = [
@@ -84,26 +58,6 @@ export default {
           map.addLayer(layer)
         }
       })
-      this.renderCanvas()
-      document.querySelector('.mapboxgl-canvas').id = 'mapbox-canvas'
-      const testSource = {
-        type: 'canvas',
-        canvas: 'uv',
-        coordinates: [
-          [1, 55], //
-          [8, 55],
-          [8, 52],
-          [1, 52]
-        ],
-        animate: true
-      }
-
-      map.addLayer({
-        id: 'test-canvas',
-        type: 'raster',
-        source: testSource,
-        paint: {}
-      })
     }
   },
   render () {
@@ -112,11 +66,3 @@ export default {
   inject: ['getMap']
 }
 </script>
-<style>
-#uv {
-  display: none;
-  position: absolute;
-  top: 0;
-  left: 0;
-}
-</style>
