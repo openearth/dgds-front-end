0efbbaa38f71c13f8a55262abc1f42bf276644b8
Vincent van Dijck <vincent@voorhoede.nl>
2020-03-30 16:29:04 +0200

    feat(tests) add new tests

---
+++ test/unit/store/preferences/index/index.spec.js
@@ -0,0 +1,10 @@
+import { state } from '../../../../../store/preferences/index.js'
+
+test('Initial state', () => {
+  const initialState = {
+    sidebarAnimating: false,
+    sidebarExpanded: false,
+    user: null
+  }
+  expect(state()).toEqual(initialState)
+})
---
+++ test/unit/store/preferences/index/mutations.spec.js
@@ -0,0 +1,37 @@
+import { mutations } from '../../../../../store/preferences/index.js'
+
+describe('setSidebarAnimating', () => {
+  test('updates state.sidebarAnimating with boolean in payload', () => {
+    const state = {
+      sidebarAnimating: false
+    }
+    const expectedResult = true
+    mutations.setSidebarAnimating(state, { animating: true })
+    expect(state.sidebarAnimating).toBe(expectedResult)
+  })
+})
+
+describe('setSidebarExpanded', () => {
+  test('updates state.sidebarExpanded with boolean in payload', () => {
+    const state = {
+      sidebarExpanded: false
+    }
+    const expectedResult = true
+    mutations.setSidebarExpanded(state, { expanded: true })
+    expect(state.sidebarExpanded).toBe(expectedResult)
+  })
+})
+
+describe('setUser', () => {
+  test('updates state.user with object in payload', () => {
+    const state = {
+      user: null
+    }
+    const expectedResult = {
+      email: 'email@email.com'
+    }
+    mutations.setUser(state, { user: { email: 'email@email.com' } })
+    expect(state.user).toMatchObject(expectedResult)
+  })
+})
+
