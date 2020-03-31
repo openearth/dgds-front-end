8b648ec2995f8eab1529c53cac3476a4121b0e8e
Vincent van Dijck <vincent@voorhoede.nl>
2020-03-31 12:02:19 +0200

    fix(auth) remove unused events

--- auth/index.js
+++ auth/index.js
@@ -6,20 +6,4 @@ Oidc.Log.level = Oidc.Log.WARN
 
 const auth = new Oidc.UserManager(oidcSettings)
 
-auth.events.addUserLoaded(() => {
-  console.log('User loaded')
-})
-
-auth.events.addAccessTokenExpiring(() => {
-  console.log('Token expiring')
-})
-
-auth.events.addAccessTokenExpired(() => {
-  console.log('Token expired')
-})
-
-auth.events.addSilentRenewError(() => {
-  console.error('Renew went wrong')
-})
-
 export default auth
