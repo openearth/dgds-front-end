33d3039b0421a822f7e61bdb05a9199dc72fffeb
Vincent van Dijck <vincent@voorhoede.nl>
2020-03-31 14:55:35 +0200

    chore(tests) remove test

--- test/unit/plugins/vuelidate.spec.js
+++
@@ -1,7 +0,0 @@
-import Vue from 'vue'
-
-describe('Vualidate plugin', () => {
-  test('installed plugins should not be empty', () => {
-    expect(Vue._installedPlugins.length).toBeGreaterThan(0)
-  })
-})
