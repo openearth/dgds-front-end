d605ce2b827cddd5f5dfba5fb697f69ae38e4af9
IoannaMi <ioannamicha@hotmail.com>
2022-01-26 22:18:03 +0100

    Add router in VueGtag

--- src/plugins/vue-gtag.js
+++ src/plugins/vue-gtag.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import VueGtag from 'vue-gtag'
+import router from '../router'
 
 Vue.use(VueGtag, {
-  config: { id: 'UA-9884034-50', anonymize_ip: true }
-})
+  config: {
+    id: 'UA-9884034-50',
+    anonymize_ip: true
+  }
+}, router)
